e73701595ca04013c09e05a1702179f2ed8a3517
Ramez Amin <ramizamin91@gmail.com>
2022-02-17 17:28:36 +0300

    first version

---
+++ src/data.js
@@ -0,0 +1,46 @@
+const data = [
+    {
+        id: 'work',
+        image: 'icon-work.svg',
+        title: 'Work',
+        time: '32hrs',
+        date: 'last week - 36hrs'
+    },
+    {
+        id: 'play',
+        image: 'icon-study.svg',
+        title: 'Play',
+        time: '10hrs',
+        date: 'last week - 8hrs'
+    },
+    {
+        id: 'study',
+        image: 'icon-study.svg',
+        title: 'Study',
+        time: '4hrs',
+        date: 'last week - 7hrs'
+    },
+    {
+        id: 'exercise',
+        image: 'icon-study.svg',
+        title: 'Exercise',
+        time: '4hrs',
+        date: 'last week - 5hrs'
+    },
+    {
+        id: 'social',
+        image: 'icon-study.svg',
+        title: 'Social',
+        time: '5hrs',
+        date: 'last week - 10hrs'
+    },
+    {
+        id: 'self-care',
+        image: 'icon-study.svg',
+        title: 'Self Care',
+        time: '2hrs',
+        date: 'last week - 2hrs'
+    },
+];
+
+export default data;
